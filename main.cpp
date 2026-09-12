6a26f384e4a18ba784b9abe2e8aa2a7480e7f335
HuiluanYie <huiluanyie@gmail.com>
2026-09-12 13:49:42 -0700

    Commit 1

--- main.cpp
+++ main.cpp
@@ -3,7 +3,12 @@
 #include <iostream>
 using namespace std;
 
+const int SIZE = 5;
+
 //Function prototype
+void enterArrayData(double*);
+void outputArrayData(double*);
+double sumArray(double*);
 
 int main() {
 
@@ -11,3 +16,26 @@ int main() {
 }
 
 //Function definition
+void enterArrayData(double *arr)
+{
+    // enterArrayData() populates the array with values that the user enters via the console
+    // arguments: the dynamic double array
+    // returns: none
+
+}
+
+void outputArrayData(double *arr)
+{
+    // outputArrayData() outputs  the array's contents on one line
+    // arguments: the dynamic double array
+    // returns: none
+
+}
+
+double sumArray(double *arr)
+{
+    // sumArray() both calculates and returns the array's sum
+    // arguments: the dynamic double array
+    // returns: the sum
+
+}
