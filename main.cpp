e690c08210c98ea4578cac5ff952426756e35357
HuiluanYie <huiluanyie@gmail.com>
2026-09-12 14:08:16 -0700

    Commit 3

--- main.cpp
+++ main.cpp
@@ -6,51 +6,55 @@ using namespace std;
 const int SIZE = 5;
 
 //Function prototype
-void enterArrayData(double*);
-void outputArrayData(double*);
-double sumArray(double*);
+void enterArrayData(double * );
+void outputArrayData(double * );
+double sumArray(double * );
 
 int main() {
-    double* dArray = nullptr;
+    double * dArray = nullptr;
     dArray = new double[SIZE];
 
+    //Function calls
     enterArrayData(dArray);
+    outputArrayData(dArray);
+    cout << "Sum of values: " << sumArray(dArray);
+
+    delete[] dArray;
 
     return 0;
 }
 
 //Function definition
-void enterArrayData(double *arr)
-{
+void enterArrayData(double * arr) {
     // enterArrayData() populates the array with values that the user enters via the console
     // arguments: the dynamic double array
     // returns: none
     cout << "Data entry for the array:\n";
-    for (int i = 0; i < SIZE; i++)
-    {
+    for (int i = 0; i < SIZE; i++) {
         cout << "\t> Element #" << i << ": ";
-        cin >> *(arr + i);
+        cin >> * (arr + i);
     }
     cout << "Data entry complete.\n";
 }
 
-void outputArrayData(double *arr)
-{
+void outputArrayData(double * arr) {
     // outputArrayData() outputs  the array's contents on one line
     // arguments: the dynamic double array
     // returns: none
     cout << "Outputting array elements: ";
-    for (int i = 0; i < SIZE; i++)
-    {
-        cout << *(arr + i) << " ";
+    for (int i = 0; i < SIZE; i++) {
+        cout << * (arr + i) << " ";
     }
     cout << endl;
 }
 
-double sumArray(double *arr)
-{
+double sumArray(double * arr) {
     // sumArray() both calculates and returns the array's sum
     // arguments: the dynamic double array
     // returns: the sum
-
+    double sum = 0;
+    for (int i = 0; i < SIZE; i++) {
+        sum += * (arr + i);
+    }
+    return sum;
 }
